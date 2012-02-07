3dbe00987a8214d7d93e06298654be0b33b8d286
Kevin Chiu <kevin.chiu@waveface.com>
2012-02-07 15:33:53 +0800

    fix ci flow

--- publishBin.py
+++ publishBin.py
@@ -9,8 +9,8 @@ repo_dir = os.path.dirname(os.path.abspath(__file__))
 if __name__ == "__main__":
     version = sys.argv[1]
     os.mkdir(version)
-    shutil.copy2('{0}\\development-WavefaceChromeExtension-{1}.crx'.format(os.path.dirname(repo_dir), version), version)
-    shutil.copy2('{0}\\production-WavefaceChromeExtension-{1}.crx'.format(os.path.dirname(repo_dir), version), version)
+    shutil.copy2('{0}\\development-WavefaceChromeExtension-{1}.crx'.format(repo_dir, version), version)
+    shutil.copy2('{0}\\production-WavefaceChromeExtension-{1}.crx'.format(repo_dir, version), version)
     ftp = FTP("WF-NAS", "admin", "waveface")
     ftp.mkd("Users/WavefaceChromeExtension/Builds/{0}".format(version))
     ftp.cwd("Users/WavefaceChromeExtension/Builds/{0}".format(version))
