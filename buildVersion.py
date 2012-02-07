4eb9c32a3fef8fa3f039467b4d6f97afac67271f
Kevin Chiu <kevin.chiu@waveface.com>
2012-02-07 15:12:33 +0800

    add ci flow

---
+++ buildVersion.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import re
+from tempfile import mkstemp
+from shutil import move
+from os import remove, close
+
+repo_dir = os.path.dirname(os.path.abspath(__file__))
+
+MANIFEST_FILES = ['WavefaceChromeExtension/manifest.json', 'WavefaceChromeExtension/manifest_dev.json']
+VER_PATTERN = re.compile('"version": "0.1"')
+
+
+def find_and_replace(target, version, pattern):
+    version = '"version": "{0}"'.format(version)
+
+    fh, abs_path = mkstemp()
+    new_file = open(abs_path, 'w')
+    old_file = open(target)
+
+    for line in old_file:
+        new_line = pattern.sub(version, line)
+        new_file.write(new_line)
+    new_file.close()
+    close(fh)
+    old_file.close()
+    remove(target)
+    move(abs_path, target)
+
+
+print "[Waveface] Replace version to {0}".format(sys.argv[1])
+for filename in MANIFEST_FILES:
+    find_and_replace(os.join(repo_dir, filename), sys.argv[1], VER_PATTERN)
+print "[Waveface] Version replacement done."
---
+++ publishBin.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import glob
+import shutil
+from ftplib import FTP
+
+repo_dir = os.path.dirname(os.path.abspath(__file__))
+
+if __name__ == "__main__":
+    version = sys.argv[1]
+    os.mkdir(version)
+    shutil.copy2('{0}/development-WavefaceChromeExtension-{1}.crx'.format(os.path.dirname(repo_dir), version), version)
+    shutil.copy2('{0}/production-WavefaceChromeExtension-{1}.crx'.format(os.path.dirname(repo_dir), version), version)
+    ftp = FTP("WF-NAS", "admin", "waveface")
+    ftp.mkd("Users/WavefaceChromeExtension/Builds/{0}".format(version))
+    ftp.cwd("Users/WavefaceChromeExtension/Builds/{0}".format(version))
+    os.chdir(version)
+    for filename in glob.glob("*".format(version)):
+        print filename
+        with open(filename, 'rb') as fp:
+            ftp.storbinary("STOR {0}".format(filename), fp)
+    ftp.close()
+    os.chdir("..")
+    shutil.rmtree(version)
