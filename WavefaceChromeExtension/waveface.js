60ba9b477f7fd478a3d91eae2a503423ce5924fd
Kevin Chiu <kevin.chiu@waveface.com>
2012-03-08 12:26:00 +0800

    open download page when first install
    1. open download page when first install
    2. remove setting page from develop version
    3. code refactor so that user cannot see develop.waveface.com
       in production version

--- WavefaceChromeExtension/waveface.js
+++ WavefaceChromeExtension/waveface.js
@@ -1,46 +1,22 @@
-function installNotice() {
-  if (localStorage.getItem("install_time")) {
-    return;
-  }
-
-  var now = new Date().getTime();
-  localStorage.setItem("install_time", now);
-
-  var endpoint = settings.get("endpoints");
-
-  if (endpoint == "development") {
-    chrome.tabs.create(url: "http://develop.waveface.com:4343/products/download");
-  }
-  else if (endpoint == "staging")
-  {
-    chrome.tabs.create(url: "http://staging.waveface.com/products/download");
-  }
-  else if (endpoint == "production")
-  {
-    chrome.tabs.create(url: "https://waveface.com/products/download");
+function installNotice(downloadUrl) {
+  var details = chrome.app.getDetails();
+  var prevVersion = localStorage["version"]
+  if (details.version != prevVersion) {
+    if (typeof prevVersion == 'undefined') {
+      console.log("Waveface Extension Installed");
+      chrome.tabs.create({url: downloadUrl});
+    } else {
+      console.log("Waveface Extension Updated");
+    }
+    localStorage["version"] = details.version;
   }
 }
 
-function runBookmarklet(tab) {
-  var endpoint = settings.get("endpoints");
-  var bookmarkletUrl;
-  
-  if (endpoint == "development") {
-    bookmarkletUrl = "javascript:(function(){ISRIL_H='c183';ISRIL_SCRIPT=document.createElement('SCRIPT');ISRIL_SCRIPT.type='text/javascript';ISRIL_SCRIPT.src='http://develop.waveface.com:4343/static/js/bookmarklet.js';document.getElementsByTagName('head')[0].appendChild(ISRIL_SCRIPT)})();";
+function runBookmarklet(bookmarkletUrl) {
+  return function(tab) {
+    chrome.tabs.update(
+      tab.id,
+      {url: bookmarkletUrl}
+    );
   }
-  else if (endpoint == "staging") {
-    bookmarkletUrl = "javascript:(function(){ISRIL_H='c183';ISRIL_SCRIPT=document.createElement('SCRIPT');ISRIL_SCRIPT.type='text/javascript';ISRIL_SCRIPT.src='http://staging.waveface.com/static/js/bookmarklet.js';document.getElementsByTagName('head')[0].appendChild(ISRIL_SCRIPT)})();"; 
-  }
-  else if (endpoint == "production") {
-    bookmarkletUrl = "javascript:(function(){ISRIL_H='c183';ISRIL_SCRIPT=document.createElement('SCRIPT');ISRIL_SCRIPT.type='text/javascript';ISRIL_SCRIPT.src='https://waveface.com/static/js/bookmarklet.js';document.getElementsByTagName('head')[0].appendChild(ISRIL_SCRIPT)})();";
-  }
-
-  chrome.tabs.update(
-    tab.id, 
-    {url: bookmarkletUrl}
-  );
 }
-
-chrome.browserAction.onClicked.addListener(runBookmarklet);
-
-installNotice()
