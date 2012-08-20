2da5f86b617f471acc6b0c78e27d0d38f503fdd7
Kudo Chien <ckchien@gmail.com>
2012-08-20 12:40:42 +0800

    Update to latest clipper.js

--- WavefaceStreamPhotoCollector/clipper.js
+++ WavefaceStreamPhotoCollector/clipper.js
@@ -15,6 +15,10 @@ var StreamBookmarklet = {
     this.streamURL = this.server_url + path + this.url
     this.streamURL += '&client=' + this.client + '&clientVer=' + this.clientVersion;
 
+		if (document.getElementById('wavefaceB')) {
+		    this.closeSelf()
+		}
+
 		this.createFrame()
 		this.setText('Saving...')
 		this.saveLink()
@@ -33,24 +37,22 @@ var StreamBookmarklet = {
 		icon_url = this.server_url + '/static/images/bookmarklet/icon.close.png'
 
 		black_overlay = document.createElement('div');
-		black_overlay.style.cssText = 'position: fixed; z-index: 999999999999;top: 0%;left: 0%;width: 100%;height: 100%;background-color: #000;-moz-opacity: 0.8;opacity:.80;filter: alpha(opacity=80);'
+		black_overlay.style.cssText = 'position: fixed; z-index: 999999999999;top: 40%;left: 50%;margin: 0 0 0 -125px;width: 250px;height: 100px;background-color: #000;-moz-opacity: 0.9;opacity:.90;filter: alpha(opacity=90); border-radius: 5px'
     black_overlay.id = 'wavefaceB'
-   	// black_overlay.addEventListener('click', this.closeSelf, false);
 
     document.body.appendChild(black_overlay)
 
 		close_icon = document.createElement('div');
-		close_icon.style.cssText = 'width:50px; height: 50px; position: absolute; z-index: 99999999999;top: 5px; right: 5px;background: url('+ icon_url +') no-repeat center center;'
+		close_icon.style.cssText = 'width:50px; height: 50px; position: absolute;top: -20px; right: -20px;background: url('+ icon_url +') no-repeat center center;'
   	close_icon.id = 'wavefaceC'
 		close_icon.addEventListener('click', this.closeSelf, false);
 
-   	document.body.appendChild(close_icon)
-
 		saving_block = document.createElement('div');
-		saving_block.style.cssText = 'width:250px; height: 20px; padding:55px 0 0;margin:0 0 0 -50px; position: absolute; z-index: 99999999999;bottom: 50%; left: 50%;color:#fff;background: url('+ logo_url +') no-repeat center top; text-align:center; font-size:18px'
+		saving_block.style.cssText = 'margin:10px; width:160px; height:60px;padding:0 0 0 70px;background: url('+ logo_url +') no-repeat left top; text-align:left; font-size:18px; color: #fff; line-height:1.4'
   	saving_block.id = 'wavefaceS'
     	
    	black_overlay.appendChild(saving_block)
+    black_overlay.appendChild(close_icon)
 
    	this.saving_block = saving_block
    					
@@ -63,7 +65,6 @@ var StreamBookmarklet = {
 	, closeSelf: function() {
 			
 		StreamBookmarklet.removeNode('wavefaceB')
-		StreamBookmarklet.removeNode('wavefaceC')
 
 	}
 	, removeNode: function(id) {
@@ -78,8 +79,9 @@ var StreamBookmarklet = {
           var text = e.target.responseText;
           var resp = JSON.parse(text);
           if (resp.web_ret_code === 0) {
-              StreamBookmarklet.setText('Saved to ' + resp.nickname + "'s Stream. <br><br>View " + '<a href="__WFLINK__/timeline" style="color:#fff; text-decoration:underline">Timeline</a>')
-              setTimeout(StreamBookmarklet.closeSelf, 3000);
+              StreamBookmarklet.setText('Photos clipped in  ' + '<a href="' + StreamBookmarklet.server_url + '/timeline" target="_blank" style="color:#fff; text-decoration:underline">' + resp.nickname + " 's " + 'collection</a>')
+
+              setTimeout(StreamBookmarklet.closeSelf, 2000);
           } else {
               window.location = resp.login_url;
           }
