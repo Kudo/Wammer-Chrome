76a3010ca324b9e4dbb32fb4d8b439e155d7221b
Kudo Chien <ckchien@gmail.com>
2012-08-31 17:20:34 +0800

    Add dev build auto update mechanism

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo "Make the deployment to develop"
 
 update_file="output/updates_dev.xml"
-crx_file="output/develop-WavefaceStreamPhotoCollector*.crx"
+crx_file="output/develop-WavefaceStreamPortal*.crx"
 
-scp $update_file wfdev:/home/wammer/static/extensions/chrome/WavefaceStreamPhotoCollector/updates_dev.xml
-scp $crx_file wfdev:/home/wammer/static/extensions/chrome/WavefaceStreamPhotoCollector/WavefaceStreamPhotoCollector.crx
+scp $update_file wfdev:/home/wammer/static/extensions/chrome/WavefaceStreamPortal/updates_dev.xml
+scp $crx_file wfdev:/home/wammer/static/extensions/chrome/WavefaceStreamPortal/WavefaceStreamPortal.crx
