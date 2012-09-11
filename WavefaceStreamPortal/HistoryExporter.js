4b42023d10dae96b088ea1591e177ef2e45c7625
Kudo Chien <ckchien@gmail.com>
2012-09-11 12:10:52 +0800

    Fix progress bar

--- WavefaceStreamPortal/HistoryExporter.js
+++ WavefaceStreamPortal/HistoryExporter.js
@@ -2,13 +2,14 @@
  * HistoryExporter - Export Chrome's all history and send to portal.
  *
  * Implementation note:
- *    Due to Chrome SDK's limitation, our implementation is too export history monthly one by one.
- *    Each month we suppose user will not have more than 2147483647 history items.
+ *    Due to Chrome SDK's limitation, our implementation is too export history daily one by one.
+ *    Everyday we suppose user will not have more than 2147483647 history items.
  *    2147483647 for maxResults is defined by Chrome SDK, which is positive part of 32-bit integer.
  *    We will export history data back to 2002.
  */
 function HistoryExporter() {
-  this.oldestDate = moment(new Date('2002/01/01'));
+  //this.oldestDate = moment(new Date('2012/01/01'));
+  this.oldestDate = moment().subtract('days', 7);
   this.wfWebUrl = "__WFLINK__";
 };
 
@@ -39,40 +40,43 @@ HistoryExporter.prototype.sendFeedData = function(histItem) {
   var qs = "api=" + encodeURIComponent('/sportal/feed') + "&data=" + encodeURIComponent(JSON.stringify(data));
 
   var xhr = new XMLHttpRequest();
-  xhr.open("POST", uri, true);
+  xhr.open("POST", uri, false);
   xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xhr.onreadystatechange = function() {
-    if (xhr.status != 200) {
-      console.error("HistoryExporter.sendFeedData() - Invalid xhr returned status. xhr.readyState[%d] xhr.status[%d]", xhr.readyState, xhr.status);
-    }
-  };
   xhr.send(qs);
+  if (xhr.status != 200) {
+    console.error("HistoryExporter.sendFeedData() - Invalid xhr returned status. xhr.readyState[%d] xhr.status[%d]", xhr.readyState, xhr.status);
+  }
 };
 
 HistoryExporter.prototype.histItemHandler = HistoryExporter.prototype.sendFeedData;
 
 HistoryExporter.prototype.exportAll = function(portalTabId) {
-  var totalCount = moment().diff(this.oldestDate, "months");
-  for (var endDate = moment(), startDate = moment(endDate).subtract("months", 1), i = 0;
+  var totalCount = moment().diff(this.oldestDate, "days");
+  for (var endDate = moment(), startDate = moment(endDate).subtract("days", 1), i = 0;
       endDate >= this.oldestDate;
-      endDate = moment(startDate).subtract("seconds", 1), startDate = moment(endDate).subtract("months", 1), ++i)
+      endDate = moment(startDate).subtract("seconds", 1), startDate = moment(endDate).subtract("days", 1), ++i)
   {
-    this.exportFromDateRange(startDate, endDate);
-
-    if (portalTabId) {
-      var progress = Math.floor(i * 100 / totalCount);
-      chrome.tabs.executeScript(portalTabId, {code: "updateHistDialogProgress(" + progress + ")"});
-    }
+    var progress = Math.floor(i * 100 / totalCount);
+    var fComplete = function(_progress) {
+      return function() {
+        if (portalTabId) {
+          chrome.tabs.executeScript(portalTabId, {code: "updateHistDialogProgress(" + _progress + ")"});
+        }
+      }
+    };
+    this.exportFromDateRange(startDate, endDate, fComplete(progress));
   }
 };
 
-HistoryExporter.prototype.exportFromDateRange = function(startDate, endDate) {
-  console.debug("HistoryExporter.exportFromDateRange(). startDate[%o] endDate[%o]", startDate, endDate);
+HistoryExporter.prototype.exportFromDateRange = function(startDate, endDate, completeHandler) {
   var histExporter = this;
   chrome.history.search({text:"", startTime: startDate.valueOf(), endTime: endDate.valueOf(), maxResults: 2147483647}, function(histItems) {
+    console.info("HistoryExporter.exportFromDateRange(). startDate[%o] endDate[%o]", startDate, endDate);
     for (var i = 0, len = histItems.length; i < len; ++i) {
       histExporter.histItemHandler(histItems[i]);
     }
+
+    if (completeHandler) { completeHandler(); }
   });
 };
